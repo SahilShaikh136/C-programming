5bbe43a12386cbe34c48261c0e053393c003e4ee
SahilShaikh136 <sahilshk@eng.rizvi.edu.in>
2025-10-17 15:43:31 +0530

    Add files via upload

---
+++ Exp1a.c
@@ -0,0 +1,6 @@
+// Name:Sahil Shaikh UIN:251P136 Computer Engineering D3 
+#include <stdio.h>
+int main(){
+    printf("Name :-Sahil Shaikh \nAge:-18 \nClass :-F.E, \ndiv:- D \nUIN:-251P136\n");
+    return 0;
+}
