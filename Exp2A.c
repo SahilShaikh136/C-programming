1bf0e6dd7f0e40fd63acba0bdfd042042dab5409
SahilShaikh136 <sahilshk@eng.rizvi.edu.in>
2025-10-17 15:46:04 +0530

    Add files via upload

---
+++ Exp2A.c
@@ -0,0 +1,13 @@
+//Sahil Shaikh Uin:-251P136 Computer Engineering D3
+#include <stdio.h>
+
+int main() {
+int num,mod;
+
+printf("Enter a number: ");
+scanf("%d",&num);
+
+mod = num%2;
+
+(mod>0? printf("The given number is odd"): printf("The given number is even"));
+return 0;}
