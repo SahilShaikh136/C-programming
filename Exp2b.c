52d74f613228ab31b99228845c84b1649acf66b5
SahilShaikh136 <sahilshk@eng.rizvi.edu.in>
2025-10-17 15:47:18 +0530

    Add files via upload

---
+++ Exp2b.c
@@ -0,0 +1,18 @@
+/*
+Name:Sahil Shaikh
+UIN:251P136      COMPUTER ENGINEERING - D2
+*/
+#include<stdio.h>
+int main(){
+     int lim;
+     int sum=0;
+     printf("enter the limit :");
+     scanf("%d",&lim);
+     for(int i=0;i<=lim;i++){
+        if(i%2!=0){
+            sum+=i;
+        }
+     }
+     printf("sum of odd is %d",sum);
+     return 0;
+}
